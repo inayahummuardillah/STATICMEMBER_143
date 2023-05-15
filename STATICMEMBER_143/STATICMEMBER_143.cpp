f785c4e75fbd19dbd1fcd0b33f54389b7c669375
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-05-15 16:26:33 +0630

    id mahasiswa akan berbeda beda

--- STATICMEMBER_143/STATICMEMBER_143.cpp
+++ STATICMEMBER_143/STATICMEMBER_143.cpp
@@ -15,3 +15,27 @@ public:
 
 int mahasiswa::nim = 0;
 
+void mahasiswa::setID() {
+	id = ++nim;
+}
+void mahasiswa::print11() {
+	cout << "ID     = " << id << endl;
+	cout << "Nama   = " << nama << endl;
+	cout << endl;
+}
+
+int main() {
+	mahasiswa mhs1("Lia Kurnia");
+	mahasiswa mhs2("Asroni");
+	mahasiswa mhs3("Andi Kurniawan");
+	mahasiswa mhs4("Joko Purbo");
+
+	mhs1.print11();
+	mhs2.print11();
+	mhs3.print11();
+	mhs4.print11();
+	return 0;
+}
+
+
+
