63fe0f77e937860e8b83214789fc5fa6e5247373
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-05-15 17:52:05 +0630

    menampilkan judul buku yang sudah diset

--- Chainfunction/Chainfunction.cpp
+++ Chainfunction/Chainfunction.cpp
@@ -16,4 +16,13 @@ public:
 	}
 } bukunya;
 
+int main()
+{
+	// bukunya.setJudul("Matematika");
+	// cout << bukunya.getJudul();
+	cout << bukunya.setJudul("Matematika").getJudul(); // chain function calls
+	return 0;
+}
+
+
 
