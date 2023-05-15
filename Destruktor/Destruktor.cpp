ba1995e40197f6528eb8233d6c43cf6810a1337d
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-05-15 18:14:32 +0630

    destruktor angka menghapus alamat array yang sudah dicetak

--- Destruktor/Destruktor.cpp
+++ Destruktor/Destruktor.cpp
@@ -11,4 +11,38 @@ public:
 	void cetakData();
 	void isiData();
 };
+//Definisi member Function
+angka::angka(int i) { //Construktor
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
+
+angka::~angka() { //Destruktor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
+void angka::cetakData() {
+	for (int i = 1;i <= panjang;i++) {
+		cout << i << " = " << arr[i] << endl;
+	}
+}
+
+void angka::isiData() {
+	for (int i = 1;i <= panjang;i++) {
+		cout << i << " = ";cin >> arr[i];
+	}
+	cout << endl;
+}
+
+int main() {
+	angka belajarcpp(3); //Constructor Dipanggil
+	angka* ptrBelajarcpp = new angka(5); //Construct Dipanggil
+	delete ptrBelajarcpp; //Detructor Dipanggil
+
+	return 0;
+}//Destructor Dipanggil
 
