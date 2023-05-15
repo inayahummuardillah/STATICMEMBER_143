e547134a77ef2a33b5b9f45ee14b815dd9524019
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-05-15 18:46:35 +0630

    membuat class mahasiswa dengan objek nim dan nama

---
+++ Konstruktor/Konstruktor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiwa {
+private:
+	int nim;
+	string nama;
+public:
+	mahasiawa();
+	mahasiswa(int);
+	mahasiswa(string);
+	mahasiswa(int iNim, string iNama);
+	void cetak();
+};
