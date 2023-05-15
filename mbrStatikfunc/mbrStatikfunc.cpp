567a8e19081c20b79fd8c56200cf7100238fa3e9
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-05-15 19:09:46 +0630

    class mahasiswa definisi function menggunakan static

---
+++ mbrStatikfunc/mbrStatikfunc.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void prinAll();
+
+	static void setNim(int pNim) { nim = pNim; /*Definisi Funcion*/ }
+	static int getNim() { return nim; /*Definisi Function*/ }
+
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
